b64adce2c61342882ed5c9dc928ec5217544ea80
Tuan Nguyen <tuan.nguyen@groovetechnology.com>
2020-08-13 10:10:54 +0700

    Add async_utils

---
+++ flask_api_utils/async_utils.py
@@ -0,0 +1,35 @@
+import asyncio
+from functools import reduce
+
+
+async def fetch_all_pages_per_url(client, method_string, **kwargs):
+    func = getattr(client, method_string)
+    if not callable(func):
+        raise TypeError('Need a callable, got %r' % func)
+
+    params = {} if not kwargs else kwargs
+
+    first = await func(**params)
+    result = await asyncio.gather(
+        *[
+            func(**{**params, **{"params": {"page": page}}})
+            for page in range(2, first["pages"] + 1)
+        ]
+    )
+    result.append(first)
+    return result
+
+
+async def fetch_all_urls(client, method_string, object_lists):
+    results = await asyncio.gather(
+        *[
+            fetch_all_pages_per_url(client, method_string, **{k: i})
+            for k, v in object_lists.items()
+            for i in v
+        ]
+    )
+    return results
+
+
+def parse_items(responses):
+    return reduce((lambda x, y: x + y.get('items', [])), responses, [])
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setuptools.setup(
     name='flask-api-utils',
-    version='1.2.1',
+    version='1.3.0',
     author="Tuan Nguyen",
     author_email="tuan.nguyen@groovetechnology.com",
     install_requires=requirements,
